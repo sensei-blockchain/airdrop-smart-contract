79ac54b3cce4ff55926fc828a420c94f3c92a9e8
sensei-blockchain <blockchaindev02@gmail.com>
2018-11-28 19:39:21 +0530

    fix

--- Airdrop.sol
+++ Airdrop.sol
@@ -88,18 +88,18 @@ contract Airdrop is Ownable {
         tokenAddr = _tokenAddr;
     }
 
-    function dropTokens(address[] _receipients, uint256[] _amount) public onlyOwner returns (bool) {
-        require(_receipients.length == _amount.length);
+    function dropTokens(address[] _recipients, uint256[] _amount) public onlyOwner returns (bool) {
+        require(_recipients.length == _amount.length);
 
-        for (uint i = 0; i < _receipients.length; i++) {
-            require(_receipients[i] != address(0));
-            require(Token(tokenAddr).transfer(_receipients[i], _amount[i]));
+        for (uint i = 0; i < _recipients.length; i++) {
+            require(_recipients[i] != address(0));
+            require(Token(tokenAddr).transfer(_recipients[i], _amount[i]));
         }
 
         return true;
     }
 
-    function dropEther(address[] _receipients, uint256[] _amount) public payable onlyOwner returns (bool) {
+    function dropEther(address[] _recipients, uint256[] _amount) public payable onlyOwner returns (bool) {
         uint total = 0;
 
         for(uint j = 0; j < _amount.length; j++) {
@@ -107,15 +107,15 @@ contract Airdrop is Ownable {
         }
 
         require(total <= msg.value);
-        require(_receipients.length == _amount.length);
+        require(_recipients.length == _amount.length);
 
 
-        for (uint i = 0; i < _receipients.length; i++) {
-            require(_receipients[i] != address(0));
+        for (uint i = 0; i < _recipients.length; i++) {
+            require(_recipients[i] != address(0));
 
-            _receipients[i].transfer(_amount[i]);
+            _recipients[i].transfer(_amount[i]);
 
-            emit EtherTransfer(_receipients[i], _amount[i]);
+            emit EtherTransfer(_recipients[i], _amount[i]);
         }
 
         return true;
